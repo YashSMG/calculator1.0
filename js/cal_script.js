d1cb7cbdaecdd0acd9fb37683a42fc018377b24d
Yash_Gadhwe <89290742+YashSMG@users.noreply.github.com>
2024-03-29 12:04:47 +0530

    Update cal_script.js
    Clear ( C ) button problem solved

--- js/cal_script.js
+++ js/cal_script.js
@@ -1,5 +1,10 @@
-function ac(ip) {
-		let a = ip.split('');
-		a.pop('');
-		return a.join('');
+function c(ip) {
+		let b = ip.split('');
+		b.pop();
+		return b.join('');
 	}
+// function clearLast() {
+//     var inputField = document.getElementsByName('inptext')[0];
+//     var currentValue = inputField.value;
+//     inputField.value = currentValue.slice(0, -1); // Remove the last character
+// }
